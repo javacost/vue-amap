6f5df6d40434d79d8f92251b3875fe14b23677f1
sanhuo <sendercheng@gmail.com>
2019-03-19 20:45:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <myamap></myamap>
+  </div>
+</template>
+
+<script>
+
+  import {AMapManager} from 'vue-amap';
+  import myamap from './components/myamap'
+  // var coordtransform = require('coordtransform/index');
+
+  export default {
+    data: function () {
+      return {}
+    },
+    components: {
+      myamap
+    }
+  }
+</script>
+
+<style>
+  .app {
+    width: 300px;
+    height: 300px;
+  }
+
+  .amap-page-container{
+    height: 100%;
+  }
+</style>
---
+++ src/components/myamap.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="amap-page-container">
+    <el-amap vid="amapDemo" :zoom="zoom" :center="center" class="amap-demo">
+      <el-amap-marker
+        v-for="(marker, index) in markers"
+        :position="marker.position"
+        :vid="index"
+        key="index"
+        >
+      </el-amap-marker>
+    </el-amap>
+    <div class="toolbar">
+      <button type="button" name="button" v-on:click="changePosition">change position</button>
+    </div>
+  </div>
+</template>
+
+<style>
+  .amap-demo {
+    height: 300px;
+  }
+</style>
+
+<script>
+  module.exports = {
+    data() {
+      return {
+        lont: 121.0,/*经度*/
+        lat: 31.0,/*纬度*/
+        lola: [],
+        zoom: 14,
+        center: [],
+        markers: [{position: []}],
+      };
+    },
+
+    created() {
+      this.changeData();
+    },
+
+    methods: {
+      changePosition() {
+        this.lont += 0.002;
+        this.lat -= 0.002;
+        this.changeData();
+      },
+
+      changeData() {
+        this.lola.push(this.lont);
+        this.lola.push(this.lat);
+        this.markers[0].position = this.center = this.lola;
+        this.lola = [];
+      }
+    }
+  };
+</script>
